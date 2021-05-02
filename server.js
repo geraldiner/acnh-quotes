4330c168f747cf46e2e6904858462ac8fde9e03a
geraldiner <hello@geraldiner.com>
2021-05-01 21:25:31 -1000

    fix errors

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ let db,
   dbConnectionStr = process.env.DB_URI,
   dbName = 'acnh-quotes'
 
-MongoClient.connect(dbConnectionStr || 'mongodb://localhost/acnh-quotes', { useUnifiedTopology: true })
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
   .then(client => {
     console.log(`Connected to ${dbName} Database`)
     db = client.db(dbName)
